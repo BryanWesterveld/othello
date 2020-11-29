37cdaaf336dee2fccac4c6d2c6ddc4fc8043ae3a
Bryan Westerveld <bryan.westerveld@gmail.com>
2020-11-29 19:35:24 +0100

    Start with diagonal indexing.

--- lib/Board.ex
+++ lib/Board.ex
@@ -7,11 +7,16 @@ defmodule OthelloEngine.Board do
     """
 
     defstruct grid: :none, history: :none
+    @indices ~w(1 2 3 4 5 6 7 8)
 
     alias OthelloEngine.Board
 
 
-    defp key(row, col) do
+    defp key(row, col) when row in @indices and col in @indices do
+        String.to_atom("#{row}#{col}")
+    end
+
+    defp key(row, col) when row in 1..8 and col in 1..8 do
         String.to_atom("#{row}#{col}")
     end
 
@@ -42,6 +47,123 @@ defmodule OthelloEngine.Board do
     """
     def start_link() do
         Agent.start_link(fn -> %Board{grid: init_grid(), history: []} end)
+    end
+
+    defp calculate_move_line_r(_cells, :none, moves) do
+        moves
+    end
+
+
+    defp calculate_move_line_r([], _color, moves) do
+        moves
+    end
+
+    defp calculate_move_line_r([{_r, _c, :none} | _tail], _color, moves) do
+        moves
+    end
+
+    defp calculate_move_line_r([{_r, _c, color} | _tail], color, moves) do
+        moves
+    end
+
+    defp calculate_move_line_r([head | tail], color, moves) do
+        calculate_move_line_r(tail, color, [head | moves])
+    end
+
+
+    defp calculate_move_line([_center | cells], color) do
+        calculate_move_line_r(cells, color, [])
+    end
+
+    defp calculate_move_horizontal(grid, row, col, color) do
+        with(
+            left <- Enum.map(col..1, fn c ->
+                        {row, c, Map.get(grid, key(row, c))}
+                    end),
+            right <- Enum.map(col..8, fn c ->
+                        {row, c, Map.get(grid, key(row, c))}
+                    end)
+        ) do
+            calculate_move_line(left, color) ++
+            calculate_move_line(right, color)
+        end
+    end
+
+    defp calculate_move_vertical(grid, row, col, color) do
+        with(
+            up <- Enum.map(row..1, fn r ->
+                        {r, col, Map.get(grid, key(r, col))}
+                    end),
+            down <- Enum.map(row..8, fn r ->
+                        {r, col, Map.get(grid, key(r, col))}
+                    end)
+        ) do
+            calculate_move_line(up, color) ++
+            calculate_move_line(down, color)
+        end
+    end
+
+    defp get_range(row, col, :top_left) when row >= col do
+        0..col-1
+    end
+
+    defp get_range(row, col, :top_left) when row < col do
+        0..row-1
+    end
+
+    defp get_range(row, col, :top_right) when row >= col do
+        # 0..8-col-1
+        0..row-1
+    end
+
+    defp get_range(row, col, :top_right) when row < col do
+        0..row-1
+    end
+
+    defp get_range(row, col, :bottom_left) when 9 - row >= col do
+        0..col-1
+    end
+
+    defp get_range(row, col, :bottom_left) when 9 - row < col do
+        0..9-row-1
+    end
+
+    defp get_range(row, col, :bottom_right) when row >= col do
+        0..9-row-1
+    end
+
+    defp get_range(row, col, :bottom_right) when row < col do
+        0..9-col-1
+    end
+
+    defp calculate_move_diagonal(grid, row, col, color) do
+        with(
+            tl <- Enum.map(get_range(row, col, :top_left), fn n ->
+                        {row - n, col - n, Map.get(grid, key(row - n, col - n))}
+                    end),
+            tr <- Enum.map(get_range(row, col, :top_right), fn n ->
+                        {row - n, col + n, Map.get(grid, key(row - n, col + n))}
+                    end),
+            bl <- Enum.map(get_range(row, col, :bottom_left), fn n ->
+                        {row + n, col - n, Map.get(grid, key(row - n, col - n))}
+                    end),
+            br <- Enum.map(get_range(row, col, :bottom_right), fn n ->
+                        {row + n, col + n, Map.get(grid, key(row - n, col - n))}
+                    end)
+        ) do
+            calculate_move_line(tl, color)
+        end
+    end
+
+    def calculate_move(board_pid, row, col, color) do
+        grid = Agent.get(board_pid, fn state -> state.grid end)
+
+        case Map.get(grid, key(row, col)) do
+            :none -> calculate_move_horizontal(grid, row, col, color) ++
+                     calculate_move_vertical(grid, row, col, color) ++
+                     calculate_move_diagonal(grid, row, col, color)
+            _     -> []
+        end
     end
 
 
