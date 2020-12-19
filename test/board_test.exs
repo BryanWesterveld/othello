c91723d9fe07aa16f748821805e200fa9377dd02
Bryan Westerveld <bryan.westerveld@gmail.com>
2020-12-19 20:53:48 +0100

    Remove references to redundant template files.

--- test/board_test.exs
+++ test/board_test.exs
@@ -1,6 +1,5 @@
 defmodule BoardTest do
     use ExUnit.Case
-    doctest OthelloEngine
 
     alias OthelloEngine.Board
 
--- test/othello_engine_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule OthelloEngineTest do
-  use ExUnit.Case
-  doctest OthelloEngine
-
-  test "greets the world" do
-    assert OthelloEngine.hello() == :world
-  end
-end
